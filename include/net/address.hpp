4675bd3dcfa2af765740c115b4927591852deee9
Dávid Kunovics <kunovicsdavid@gmail.com>
2025-09-10 21:10:37 +0200

    string_view instead of raw pointer

--- include/net/address.hpp
+++ include/net/address.hpp
@@ -4,66 +4,73 @@
 #include <net/utils/parse.hpp>
 
 #include <array>
+#include <expected>
+#include <string_view>
 
 namespace net {
     struct IPv4Address {
         std::array<u8, 4U> address = { 0U, 0U, 0U, 0U };
         u16 port = 0U;
+
+        [[nodiscard]] static constexpr auto parse(
+            std::string_view str
+        ) noexcept -> std::expected<IPv4Address, ParseError>;
     };
+
+    constexpr auto IPv4Address::parse(std::string_view str) noexcept -> std::expected<IPv4Address, ParseError> {
+        IPv4Address v4{};
+
+        for(u8 octet = 0U; octet < 4U; ++octet) {
+            const auto address = parse_uint<u8>(str, 3U, { '.', ':' });
+            if(!address) {
+                return std::unexpected(address.error());
+            }
+
+            if(octet < 3U) {
+                if(str.front() != '.') {
+                    return std::unexpected(ParseError::InvalidCharacter);
+                }
+
+                str.remove_prefix(1U);
+            }
+
+            v4.address[octet] = address.value();
+        }
+
+        if(str.empty()) {
+            return v4;
+        }
+
+        if(str.front() != ':') {
+            return std::unexpected(ParseError::InvalidCharacter);
+        }
+
+        str.remove_prefix(1U);
+
+        const auto port = parse_uint<u16>(str, 5U);
+        if(!port) {
+            return std::unexpected(port.error());
+        }
+
+        v4.port = port.value();
+
+        if(!str.empty()) {
+            return std::unexpected(ParseError::InvalidCharacter);
+        }
+
+        return v4;
+    }
 
     namespace literals {
         constexpr auto operator""_v4(const char* str, usize length) -> IPv4Address;
 
         constexpr auto operator""_v4(const char* str, [[maybe_unused]] const usize length) -> IPv4Address {
-            IPv4Address v4{};
-
-            for(u8 octet = 0U; octet < 4U; ++octet) {
-                const auto address_result = parse_uint(str, 3U, { '.', ':' });
-                if(!address_result) {
-                    throw;
-                }
-
-                const auto address = address_result.value();
-                if(address > 0xFFU) {
-                    throw;
-                }
-
-                if(octet < 3U) {
-                    if(*str != '.') {
-                        throw;
-                    }
-
-                    ++str;
-                }
-
-                v4.address[octet] = address;
-            }
-
-            if(*str == '\0') {
-                return v4;
-            }
-
-            if(*str++ != ':') {
+            const auto address = IPv4Address::parse(str);
+            if(!address) {
                 throw;
             }
 
-            const auto port_result = parse_uint(str, 5U);
-            if(!port_result) {
-                throw;
-            }
-
-            const auto port = port_result.value();
-            if(port > 0xFFFFU) {
-                throw;
-            }
-
-            v4.port = port;
-
-            if(*str != '\0') {
-                throw;
-            }
-
-            return v4;
+            return address.value();
         }
     }
 }
--- include/net/utils/parse.hpp
+++ include/net/utils/parse.hpp
@@ -6,6 +6,7 @@
 #include <expected>
 #include <initializer_list>
 #include <limits>
+#include <string_view>
 
 namespace net {
     enum class ParseError {
@@ -15,33 +16,39 @@ namespace net {
 
     template<std::unsigned_integral T>
     constexpr auto parse_uint(
-        const char*& str,
+        std::string_view& str,
         usize max_length,
         std::initializer_list<char> delimiters = {}
     ) noexcept -> std::expected<T, ParseError>;
 
     template<std::unsigned_integral T>
     constexpr auto parse_uint(
-        const char*& str,
+        std::string_view& str,
         const usize max_length,
         const std::initializer_list<char> delimiters
     ) noexcept -> std::expected<T, ParseError> {
-        if(*str < '0' || *str > '9') {
+        if(str.empty() || str.front() < '0' || str.front() > '9') {
             return std::unexpected(ParseError::InvalidCharacter);
         }
 
         usize value = 0U;
         for(usize i = 0U; i < max_length; ++i) {
-            const char c = *str;
+            if(str.empty()) {
+                break;
+            }
+
+            const char c = str.front();
 
             bool is_delimiter = false;
             for(const auto d : delimiters) {
                 if(c == d) {
                     is_delimiter = true;
+
+                    break;
                 }
             }
 
-            if(is_delimiter || c == '\0') {
+            if(is_delimiter) {
                 break;
             }
 
@@ -51,7 +58,7 @@ namespace net {
 
             value = value * 10U + (c - '0');
 
-            ++str;
+            str.remove_prefix(1U);
         }
 
         if(value > std::numeric_limits<T>::max()) {
