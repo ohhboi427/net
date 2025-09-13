fb09b3ab6a2c48c3b4db24ebeca06b587bfe681e
Dávid Kunovics <kunovicsdavid@gmail.com>
2025-09-13 21:57:14 +0200

    Data transport utils

--- include/net/tcp/stream.hpp
+++ include/net/tcp/stream.hpp
@@ -16,8 +16,10 @@ namespace net {
         [[nodiscard]] static auto connect(IPv4Address address) noexcept -> std::expected<TcpStream, SocketError>;
 
         [[nodiscard]] auto write(std::span<const u8> data) const noexcept -> std::expected<usize, SocketError>;
+        [[nodiscard]] auto write_all(std::span<const u8> data) const noexcept -> std::expected<void, SocketError>;
 
         [[nodiscard]] auto read(std::span<u8> buffer) const noexcept -> std::expected<usize, SocketError>;
+        [[nodiscard]] auto read_exact(std::span<u8> buffer) const noexcept -> std::expected<void, SocketError>;
 
     private:
         std::unique_ptr<void, void(*)(void*)> m_impl{ nullptr, nullptr };
---
+++ src/tcp/stream.cpp
@@ -0,0 +1,33 @@
+#include <net/tcp/stream.hpp>
+
+namespace net {
+    auto TcpStream::write_all(const std::span<const u8> data) const noexcept -> std::expected<void, SocketError> {
+        usize all_bytes_sent = 0U;
+
+        while(all_bytes_sent < data.size()) {
+            const auto bytes_sent = write(data.subspan(all_bytes_sent));
+            if(!bytes_sent) {
+                return std::unexpected(bytes_sent.error());
+            }
+
+            all_bytes_sent += bytes_sent.value();
+        }
+
+        return {};
+    }
+
+    auto TcpStream::read_exact(const std::span<u8> buffer) const noexcept -> std::expected<void, SocketError> {
+        usize all_bytes_received = 0U;
+
+        while(all_bytes_received < buffer.size()) {
+            const auto bytes_received = read(buffer.subspan(all_bytes_received));
+            if(!bytes_received) {
+                return std::unexpected(bytes_received.error());
+            }
+
+            all_bytes_received += bytes_received.value();
+        }
+
+        return {};
+    }
+}
